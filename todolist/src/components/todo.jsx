dfc441717f5e8292334747e2a019b32c178cfe5b
Facu <facundo.orlando@acamica.com>
2021-03-06 21:34:45 -0300

    Add edit task function

--- todolist/src/components/todo.jsx
+++ todolist/src/components/todo.jsx
@@ -5,9 +5,11 @@ const ToDo = () => {
 
     const [task, setTask] = useState('');
     const [taskList, setTasksList] = useState([]);
+    const [editionMode, setEditionMode] = useState(false);
+    const [id, setId] = useState('');
 
     const addTask = (e) => {
-        e.preventDefault(); // para que no se refresque la página al hacer submit
+        e.preventDefault(); 
         const newTask = {
             id: uniqid(),
             title: task
@@ -15,10 +17,19 @@ const ToDo = () => {
         setTasksList([...taskList, newTask])
         setTask('')
     }
-
     const deleteTask = (id) => {
         const newTaskList = taskList.filter(item => item.id !== id)
         setTasksList(newTaskList);
+    }
+    const editMode = (item) => {
+        setEditionMode(true);
+        setTask(item.title);
+        setId(item.id);
+    }
+    const editTask = (e) => {
+        e.preventDefault();
+        const newEditTaskList = taskList.map(item => item.id === id ? { id: id, title: task } : item);
+        setTasksList(newEditTaskList);
     }
 
     return (
@@ -26,31 +37,36 @@ const ToDo = () => {
             <h2>To Do List</h2>
             <div className="row">
                 <div className="col">
-                    <form onSubmit={(e) => addTask(e)} action="" className="form-group">
+                    <form onSubmit={editionMode ? editTask : addTask} action="" className="form-group">
                         <h3>Agrega una tarea</h3>
                         <input
-                            onChange={(e) => {
-                            setTask(e.target.value)
-                        }}
+                            onChange={(e) => { setTask(e.target.value) }}
                             type="text"
                             className="form-control mb-3"
                             placeholder="Agrega una nueva tarea"
                             value={task}/>
-                        <input type="submit" className="btn btn-block btn-info" value="Agregar tarea"/>
+                        <input
+                            type="submit"
+                            className="btn btn-block btn-info"
+                            value={editionMode ? "Editar tarea" : "Agregar tarea"}/>
                     </form>
                 </div>
                 <div className="col">
                     <h3>Listado de tareas</h3>
                     <ul className="list-group">
-                        {
-                            taskList.map(item => 
-                                <li key={item.id} className="list-group-item">
-                                    {item.title}
-                                    <button className="btn btn-danger float-right" onClick={() => {deleteTask(item.id) } }>X</button>
-                                </li>
-                            )
-                        }
-
+                        {taskList.map(item => 
+                            <li key={item.id} className="list-group-item">
+                                {item.title}
+                                <button
+                                    className="btn btn-danger float-right"
+                                    onClick={() => { deleteTask(item.id) }}>X
+                                </button>
+                                <button
+                                    className="btn btn-info float-right"
+                                    onClick={() => { editMode(item) }}>Edit
+                                </button>
+                            </li>
+                        )}
                     </ul>
                 </div>
 
